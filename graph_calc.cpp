600fd16242bbc3d705d42306d684df1606823bd9
Isengrom <bdwark01@louisville.edu>
2024-10-03 14:54:59 -0400

    graph_calc branch first commit

---
+++ graph_calc.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector> 
+
+class Polyfunc {
+public:
+
+
+
+};
+
+vector<double> graph_helper(Polyfunc funcobj, double posX, double negX, double posY, double negY){
+
+}
+
+void parseFunc(string func){
+    
+}
+
+vector<double> graph_calc(string func, double posX, double negX, double posY, double negY){
+    Polyfunc funcobj;
+    parseFunc(func);
+    
+
+
+
+    return graph_helper(Polyfunc funcobj, double posX, double negX, double posY, double negY);
+}
