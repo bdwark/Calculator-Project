d2db6c0fb52e5e21d126c7e92a82dc5d8dbb07d9
Isengrom <bdwark01@louisville.edu>
2024-11-16 15:12:46 -0500

    fixed bugs

--- graph_calc.cpp
+++ graph_calc.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include <vector> 
-#include <graph_calc.h>
+#include <Polyfunc.h>
+using namespace std;
 
 vector<int> xPointsListFinder(int leftX, int rightX){
     vector <int> xPoints;
     int xSpan = rightX - leftX;
     double inc = xSpan/1080;
     for(int i = 0; i <= 1080; i++){
-            xPoints.pushback(leftX + inc*i);
+            xPoints.push_back(leftX + inc*i);
     }
     return xPoints;
 }
 
 
-vector<Polyfunc> graph_calc(double a, double b, double c, double leftX, double rightX, double bottomY, double topY){
+vector<Polyfunc> graphCalc(double a, double b, double c, double leftX, double rightX, double bottomY, double topY){
     vector <int> xPointsList = xPointsListFinder(leftX, rightX);
     vector <Polyfunc> pointList;
-    for(int i == 0; i <= xPointsList.size(); i++){
+    for(int i = 0; i <= xPointsList.size(); i++){
         double y = a*(xPointsList(i))^2 + b*xPointsList(i) + c;
         Polyfunc pointObj(xPointsList(i), y);
-        pointList.pushback(pointObj);
+        pointList.push_back(pointObj);
     }
 
 
--- graph_calc.h
+++
@@ -1,4 +0,0 @@
-#include <Polyfunc.h>
-
-vector<int> xPointsListFinder(int leftX, int rightX);
-vector<Polyfunc> graph_calc(double a, double b, double c, double leftX, double rightX, double topY, double bottomY);
