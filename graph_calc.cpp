534840274587cef24c4fa8158d769231671fafe8
Isengrom <bdwark01@louisville.edu>
2024-11-16 17:13:29 -0500

    minor fix

--- graph_calc.cpp
+++ graph_calc.cpp
@@ -16,7 +16,7 @@ vector<int> xPointsListFinder(double leftX, double rightX) {
 vector<Polyfunc> graphCalc(double a, double b, double c, double leftX, double rightX){
     vector<int> xPointsList = xPointsListFinder(leftX, rightX);
     vector<Polyfunc> pointList;
-    for(int i = 0; i <= xPointsList.size(); i++){
+    for(int i = 0; i <= xPointsList.size()-1; i++){
         double y = a*(xPointsList[i]^2) + b*xPointsList[i]+ c;
         Polyfunc pointObj(xPointsList[i], y);
         pointList.push_back(pointObj);
