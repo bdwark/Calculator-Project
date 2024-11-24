4ae256dc7125a9bbda771266cbf6561928d60a83
Quinlan Toole <54750790+quinlantoole@users.noreply.github.com>
2024-11-24 08:05:45 -0500

    Update AngleConversion.h

--- ConversionsCode/AngleConversion.h
+++ ConversionsCode/AngleConversion.h
@@ -6,13 +6,6 @@
 
 using namespace std;
 
-/*
-    Angle Conversions
-    Returns floats
-*/
-
-_DECL float pi = 2 * acos(0);
-
 float degreesToRadians (float val);
 
 float radiansToDegrees (float val);
