ed6f0425db307b23e0c2747b82e43d11fa50dad9
Sam S. <ssmallwood617@gmail.com>
2024-12-02 14:10:44 -0500

    Delete GraphingCode directory

--- GraphingCode/graph_calc.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-using namespace std;
-
-#include "Polyfunc.h"
-#include <vector>
-
-// Function declarations
-vector<double> xPointsListFinder(double tempLeftX, double tempRightX, int pixels);
-vector<Polyfunc> graphCalc(double a, double b, double c, double leftX, double rightX, int pixels);
