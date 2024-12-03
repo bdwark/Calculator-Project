2b97b26d19efa4c18b0d6c88c6108e83be5fcafa
elimars05 <118490809+elimars05@users.noreply.github.com>
2024-12-03 06:11:30 -0500

    main.cpp

--- mathbasics/main.cpp
+++ mathbasics/main.cpp
@@ -2,29 +2,29 @@
 #include "Calculator.h"
 
 int main() {
-    Calculator calc;
-    int option;
+    Calculator calc; // makes an instance of the of the Calculator class to perform operations
+    int option; // holds the user's numerical input for their choice in the menu prompted below
 
     while (true) {
-        displayMainMenu();
-        std::cin >> option;
+        displayMainMenu(); // displays the main menu in a loop unless the 3rd option to leave the program is selected
+        std::cin >> option; // gets the user's choice
 
         switch (option) {
-            case 1:
+            case 1: // calls the 'BasicOperations' function to handle basic math operations like addition or multiplication
                 handleBasicOperations(calc);
                 break;
-            case 2:
+            case 2: // calls the 'StatisticalOperations' function to handle operations for multiple values like mean or median
                 handleStatisticalOperations(calc);
                 break;
-            case 3:
+            case 3: // ends the program
                 std::cout << "Exiting program.\n";
                 return 0;
-            default:
+            default: // given if an invalid option is picked, prompts the user to try again
                 std::cout << "Invalid option. Please try again.\n";
         }
     }
 
-    return 0;
+    return 0; 
 }
 
 
