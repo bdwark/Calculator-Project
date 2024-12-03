e21690f3e1399db7efc4080695e220dcf3cdb6a4
elimars05 <118490809+elimars05@users.noreply.github.com>
2024-12-03 06:19:36 -0500

    Menu.h

--- mathbasics/Menu.h
+++ mathbasics/Menu.h
@@ -4,9 +4,9 @@
 #include "Calculator.h"
 
 // Function declarations
-void displayMainMenu();
-void handleBasicOperations(Calculator& calc);
-void handleStatisticalOperations(Calculator& calc);
+void displayMainMenu(); // displays the options in the main menu 
+void handleBasicOperations(Calculator& calc); // holds the functions of the basic math operations like multiplication, additions or division
+void handleStatisticalOperations(Calculator& calc); // holds the functions of the statiscal operations to find stuff like the mean or median of the multiple values
 
 #endif
 
