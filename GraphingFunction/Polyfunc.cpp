4d95a85036324a4d2dc7b8033bcd885c431b775a
Eve W <151676968+bdwark@users.noreply.github.com>
2024-12-05 11:52:38 -0500

    Update Polyfunc.cpp

--- GraphingFunction/Polyfunc.cpp
+++ GraphingFunction/Polyfunc.cpp
@@ -1,9 +1,11 @@
 #include "Polyfunc.h"
 
+//Polyfunc constructor 
 Polyfunc::Polyfunc(double tempX, double tempY){
     x = tempX; 
     y = tempY; 
 }
 
+//two functions for geting the X and Y data
 double Polyfunc::getX() { return x; }
 double Polyfunc::getY() { return y; }
