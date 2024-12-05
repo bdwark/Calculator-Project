0e6f6e64cbcd2095fd8a9c963f5545e368239f68
Eve W <151676968+bdwark@users.noreply.github.com>
2024-12-05 11:49:52 -0500

    Update Graph_calc.cpp

--- GraphingFunction/Graph_calc.cpp
+++ GraphingFunction/Graph_calc.cpp
@@ -3,6 +3,9 @@
 #include <cmath>
 #include "Graph_calc.h"
 
+//This takes in the left and right X bounds and the pixel width of the screan
+//then it finds the span of numbers it must calculate and finds the value to increment x by
+//then it generates a vector of all the x points
 std::vector<double> xPointsListFinder(double leftX, double rightX, int pixels) {
     std::vector <double> xPoints;
     double xSpan = rightX - leftX;
@@ -13,6 +16,10 @@ std::vector<double> xPointsListFinder(double leftX, double rightX, int pixels) {
     return xPoints;
 }
 
+//this takes in the polynomial function and the left and right x bounds, and the pixel width of the screen
+//it then parses these numbers and calls xPointsListFinder to return the list of x points
+//it then takes these x points and calculates the y points
+//then it creates the point
 std::vector<Polyfunc> graphCalc(double a, double b, double c, double leftX, double rightX, int pixels){
     std::vector<double> xPointsList = xPointsListFinder(leftX, rightX, pixels);
     std::vector<Polyfunc> pointList;
