0b0e257fb972ce5b0ef80943446d389c998e1181
Eve W <151676968+bdwark@users.noreply.github.com>
2024-12-05 11:52:00 -0500

    Update Graph_calc.cpp

--- GraphingFunction/Graph_calc.cpp
+++ GraphingFunction/Graph_calc.cpp
@@ -4,6 +4,9 @@
 #include "Graph_calc.h"
 using namespace std;
 
+//This takes in the left and right X bounds and the pixel width of the screan
+//then it finds the span of numbers it must calculate and finds the value to increment x by
+//then it generates a vector of all the x points
 vector<double> xPointsListFinder(double leftX, double rightX, int pixels) {
     vector <double> xPoints;
     double xSpan = rightX - leftX;
@@ -14,7 +17,11 @@ vector<double> xPointsListFinder(double leftX, double rightX, int pixels) {
     return xPoints;
 }
 
-
+//this takes in the polynomial function and the left and right x bounds, and the pixel width of the screen
+//it then parses these numbers and calls xPointsListFinder to return the list of x points
+//it then takes these x points and calculates the y points
+//then it creates a pointObj of class polyfunc, which is just a class which contains the point data
+//then after adding these points to a vector, it returns this vector 
 vector<Polyfunc> graphCalc(double a, double b, double c, double leftX, double rightX, int pixels){
     vector<double> xPointsList = xPointsListFinder(leftX, rightX, pixels);
     vector<Polyfunc> pointList;
@@ -23,7 +30,5 @@ vector<Polyfunc> graphCalc(double a, double b, double c, double leftX, double ri
         Polyfunc pointObj(xPointsList[i], y);
         pointList.push_back(pointObj);
     }
-
-
     return pointList;
 }
